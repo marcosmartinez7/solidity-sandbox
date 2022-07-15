cad237b92c93201dc2cef877ac102e5443592df3
Marcos Martínez <mmartinezciompi@gmail.com>
2022-07-14 21:44:08 -0300

    Conventions

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,27 +2,39 @@
 pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "./PriceConverter.sol";
 
-error NotEnoughFunds();
+error FundMe_NotEnoughFunds();
 
+/// @title A contract for crowd funding
+/// @author marcosmartinez7
+/// @notice This contract is to demo a sample funding contract
+/// @dev This implements price feeds as our library and uses chainlink oracles.
 contract FundMe is Ownable {
     using PriceConverter for uint256;
+
     uint256 public constant MINIMUM_USD = 50 * 1e18;
-
-    AggregatorV3Interface public priceFeed;
-
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+    AggregatorV3Interface public priceFeed;
 
     constructor(address priceFeedAddress) {
         priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    /// @notice This funtion funds this contract
+    /// @dev This function checks against price feeds and add sender to funders.
     function fund() public payable {
         if (msg.value.getConversionRate(priceFeed) < MINIMUM_USD) {
-            revert NotEnoughFunds();
+            revert FundMe_NotEnoughFunds();
         }
 
         funders.push(msg.sender);
@@ -45,13 +57,5 @@ contract FundMe is Ownable {
             value: address(this).balance
         }("");
         require(callSuccess, "Call failed");
-    }
-
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
     }
 }
